f41deb4abc49271b60770eed6347e1433031a35f
Carlos Blanco <carlosblancoh@usal.es>
2022-09-07 02:10:19 +0200

    Add storybook

--- src/hooks/previous.js
+++
@@ -1,8 +0,0 @@
-import { useEffect, useRef } from 'react';
-
-
-export default function usePrevious(value) {
-    const ref = useRef();
-    useEffect(() => void (ref.current = value), [value]);
-    return ref.current;
-}
--- src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { itemSort } from './itemSort';
-export { hexToRgb } from './hexToRgb';
-export { groupsDataOrganize } from './groupsDataOrganize';
